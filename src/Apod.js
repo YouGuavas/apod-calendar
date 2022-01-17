6461e6bf640f368ff8fc7bbee7149df51a750554
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-16 20:19:22 -0500

    Add picture of the day

--- src/Apod.js
+++ src/Apod.js
@@ -1,8 +1,16 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Apod(props) {
+  const [data, setData] =  useState('');
+  useEffect(() => {
+    axios(`https://api.nasa.gov/planetary/apod?api_key=${props.api_key}`)
+      .then(response => setData(response.data))
+      .catch(error => console.log(error))
+  })
   return(
     <div>
-      hi
-      {props.api_key}
+      <img src={data.url} />
     </div>
   )
 }
